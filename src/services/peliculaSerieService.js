317e2f1f3880759cabf99dd577b3fc6fe3bcdfff
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-06-07 12:05:03 -0300

    Trantando de arreglar el ultimo endpoint

--- src/services/peliculaSerieService.js
+++ src/services/peliculaSerieService.js
@@ -43,7 +43,8 @@ export const getByParams = async (nombre, orden) =>{
 
 export const getByID = async (numero) => {
     const conn = await sql.connect(configDB);
-    const results = await conn.request().input("whereCondition", numero).query("SELECT ps.Id, ps.Imagen, ps.Titulo, ps.FechaCreacion, ps.Calificacion, STRING_AGG(p.Nombre + ' (Edad: ' + Convert(VARCHAR(MAX),p.Edad) + ' Peso: ' + Convert(VARCHAR(MAX),p.Peso) + ' Imagen: ' + p.Imagen + ' Historia: ' + p.Historia + ')' , '; ') AS Personajes FROM PeliculaSerie ps INNER JOIN PersonajeXPeliculaSerie pxp ON ps.Id = pxp.IdPeliculaSerie INNER JOIN Personaje p ON pxp.IdPersonaje = p.Id WHERE ps.Id = @whereCondition GROUP BY ps.Id, ps.Imagen, ps.Titulo, ps.FechaCreacion, ps.Calificacion");
+    const results = await conn.request().input("whereCondition", numero).query("SELECT ps.Id, ps.Imagen, ps.Titulo, ps.FechaCreacion, ps.Calificacion, STRING_AGG(p.Nombre + ' (Edad: ' + Convert(VARCHAR(MAX),p.Edad) + ' Peso: ' + Convert(VARCHAR(MAX),p.Peso) + ' Imagen: ' + p.Imagen + ' Historia: ' + p.Historia + ')' , ' ; ') AS Personajes FROM PeliculaSerie ps INNER JOIN PersonajeXPeliculaSerie pxp ON ps.Id = pxp.IdPeliculaSerie INNER JOIN Personaje p ON pxp.IdPersonaje = p.Id WHERE ps.Id = @whereCondition GROUP BY ps.Id, ps.Imagen, ps.Titulo, ps.FechaCreacion, ps.Calificacion");
+    results.Personajes.split(';')
     console.log(results)
     return results
 }
